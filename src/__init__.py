2c4cf8439458ff71c6e9cc728b61b7d7252f68d5
Jayme Gordon <jaymegordo@gmail.com>
2021-04-02 13:04:29 -0700

    Create download script

---
+++ src/__init__.py
@@ -0,0 +1,32 @@
+
+import logging
+
+
+def getlog(name: str):
+    """Create logger object with predefined stream handler & formatting
+
+    Parameters
+    ----------
+    name : str
+        module __name__
+
+    Returns
+    -------
+    logging.logger
+
+    Examples
+    --------
+    >>> from .__init__ import getlog
+    >>> log = getlog(__name__)
+    """
+    fmt_stream = logging.Formatter('%(levelname)-7s %(lineno)-4d %(name)-26s %(message)s')
+    sh = logging.StreamHandler()
+    sh.setLevel(logging.INFO)
+    sh.setFormatter(fmt_stream)
+
+    log = logging.getLogger(name)
+    if not log.handlers:
+        log.setLevel(logging.INFO)
+        log.addHandler(sh)
+
+    return log
