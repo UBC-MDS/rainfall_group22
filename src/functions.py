6ad3b8382191034b58df87b700e1bf5439231851
Jayme Gordon <jaymegordo@gmail.com>
2021-04-03 13:02:48 -0700

    Add helper funcs

--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,5 @@
 from seaborn import diverging_palette
+from pathlib import Path
 
 _cmap = diverging_palette(240, 10, sep=10, n=21, as_cmap=True)
 
@@ -11,3 +12,21 @@ def bg(style, subset=None, rev=True, axis=0):
 
     return style \
         .background_gradient(cmap=cmap, subset=subset, axis=axis)
+
+def calc_size(p : Path, nice=True):
+    """Calculate size of directory and all subdirs
+
+    Parameters
+    ----------
+    p : Path
+        [description]
+    nice : bool, optional
+        return raw float or nicely formatted string, default False
+
+    Returns
+    -------
+    int | string
+        size of folder
+    """    
+    _size = sum(_p.stat().st_size for _p in p.glob('**/*') if _p.is_file())
+    return _size
