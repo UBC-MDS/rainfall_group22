e669d4e0c347be4e6ca0681ad37c0f8ef915e70e
Jayme Gordon <jaymegordo@gmail.com>
2021-04-29 18:09:04 -0700

    Add extra error handling

--- app.py
+++ app.py
@@ -36,11 +36,35 @@ def handle_exception(err):
     return jsonify(response), 500
 
 
-def return_prediction(input_vals: list=None) -> float:
+def return_prediction(input_vals: list) -> float:
+    """Return model prediction
 
+    Parameters
+    ----------
+    input_vals : list
+        list of length 25 for model to predict
+
+    Returns
+    -------
+    float
+        model's predicted value
+
+    Raises
+    ------
+    APIError
+        incorrect input type
+    APIError
+        wrong length list
+    """    
     if not isinstance(input_vals, list):
         raise APIError(
             f'Input must be of type list|ndarray, you input: ({type(input_vals)}, {input_vals})')
+    
+    len_vals = len(input_vals)
+    if not len_vals == 25:
+        raise APIError(
+            f'Input array must be of length 25, your array length: {len_vals}'
+        )
 
     x = np.array(input_vals).reshape(1, len(input_vals))
     return model.predict(x)[0]
@@ -49,7 +73,6 @@ def return_prediction(input_vals: list=None) -> float:
 def index():
     header = 'Welcome to our rain prediction service'
 
-    # <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='style.css') }}">
     body = Markup("""
     <h3>525 Group 22</h3>
 
@@ -57,7 +80,7 @@ def index():
     Requests with an empty array will return a prediction for 25 random values.<br><br>
     
     eg:
-    <pre>curl -X POST ec2_ip_address/predict -d '{"data":[1,2,3,4,53,11,22,37,41,53,11,24,31,44,53,11,22,35,42,53,12,23,31,42,53]} -H "Content-Type: application/json"<br>
+    <pre>curl -X POST http://<ec2_ip_address>:8080/predict -d '{"data":[1,2,3,4,53,11,22,37,41,53,11,24,31,44,53,11,22,35,42,53,12,23,31,42,53]} -H "Content-Type: application/json"<br>
 {
     "input_vals": [1, 2, 3, 4, 53, 11, 22, 37, 41, 53, 11, 24, 31, 44, 53, 11, 22, 35, 42, 53, 12, 23, 31, 42, 53], 
     "predicted_rainfall": "31.59mm"
@@ -75,12 +98,17 @@ def rainfall_prediction():
     input_vals = None
 
     if not content is None:
-        input_vals = list(content.values())[0]
+        if isinstance(content, dict) and len(content) > 0:
+            input_vals = list(content.values())[0]
+        else:
+            raise APIError('Input data incorrect format.')
+    else:
+        raise APIError('Missing input data.')
 
     # allow blank input, just use random numbers for input_vals
     if (
         input_vals is None
-        or (isinstance(input_vals, list) and not len(input_vals) == 25)):
+        or (isinstance(input_vals, list) and len(input_vals) == 0)):
 
         input_vals = np.random.randint(0, 250, 25).tolist()
 
